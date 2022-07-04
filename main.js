26a09b441e1de730c8613c4dc3a7631135c9a38a
delivey <deliveydev@gmail.com>
2022-07-04 11:44:03 +0300

    PGN with no bugs finished

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 function getMoves() {
     moves = document.getElementsByClassName('move');
     idx=1
-    order = []
     PGN = ""
     for (let move of moves) {
         let madeMoves = move.children
         let moves = []
         for (let madeMove of madeMoves) {
-            if (idx == 1) order.push(madeMove.classList[0])
-            moves.push(madeMove.innerText)
+            let moving = ""
+            try {
+                moving += madeMove.children[0].dataset.figurine
+            } catch (e) {}
+            moving += madeMove.innerText
+            moves.push(moving)
         }
         PGN += `${idx}. ${moves[0]} ${moves[1]} `
         idx++
