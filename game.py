296fa0a6442f3ee127880344ba903900887b6ee4
delivey <deliveydev@gmail.com>
2022-07-08 12:21:07 +0300

    Some highlighting finished

--- game.py
+++ game.py
@@ -3,55 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from engine import get_best_move
+from javascript import JS_getPgn, JS_highlightSquare
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.chess.com/play/computer")
 
-finish = input("Finished setup? ")
+finish = input("Finished setup?\n")
 
-triggerMouseEvent(targetNode, "mouseover");
-triggerMouseEvent(targetNode, "mousedown");
-triggerMouseEvent(targetNode, "mouseup");
-triggerMouseEvent(targetNode, "click");
-
-JS_getPGN = """
-moves = document.getElementsByClassName('move');
-idx=1
-PGN = ""
-for (let move of moves) {
-    let madeMoves = move.children
-    let moves = []
-    for (let madeMove of madeMoves) {
-        let moving = ""
-        try {
-            moving += madeMove.children[0].dataset.figurine
-        } catch (e) {}
-        moving += madeMove.innerText
-        moves.push(moving)
-    }
-    PGN += `${idx}. ${moves[0]} ${moves[1]} `
-    idx++
-}
-return PGN
-"""
-
-pgn = driver.execute_script(JS_getPGN)
+pgn = driver.execute_script(JS_getPgn)
 move = get_best_move(pgn)
 print(move)
-
-pwn = driver.find_element(By.CSS_SELECTOR, f'div[class="piece wp square-22"]')
-pwn.click()
 
 idx = 1
 while True:
     idx += 1
     try:
         next = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, f'div[data-ply="{idx*2}"]')))
-        pgn = str(driver.execute_script(JS_getPGN)).rstrip().lstrip()
+        pgn = str(driver.execute_script(JS_getPgn)).rstrip().lstrip()
         move = get_best_move(pgn)
+        squares = [move[i:i+2] for i in range(0, len(move), 2)]
         print(move)
+        print(squares)
+        driver.execute_script(JS_highlightSquare(squares[0], "blue"))
+        driver.execute_script(JS_highlightSquare(squares[1], "red"))
     except KeyboardInterrupt:
         break
 
---
+++ javascript.py
@@ -0,0 +1,43 @@
+import string
+
+JS_getPgn = """
+moves = document.getElementsByClassName('move');
+idx=1
+PGN = ""
+for (let move of moves) {
+    let madeMoves = move.children
+    let moves = []
+    for (let madeMove of madeMoves) {
+        let moving = ""
+        try {
+            moving += madeMove.children[0].dataset.figurine
+        } catch (e) {}
+        moving += madeMove.innerText
+        moves.push(moving)
+    }
+    PGN += `${idx}. ${moves[0]} ${moves[1]} `
+    idx++
+}
+return PGN
+"""
+
+def convertSquare(square):
+    first = str(string.ascii_lowercase.index(square[0]) + 1)
+    second = str(square[1])
+    print(first+second)
+    return first + second
+
+def JS_highlightSquare(square, color):
+    num = convertSquare(square)
+    js = f"""
+    const newSquare = document.createElement("div");
+    newSquare.setAttribute("id", {num});
+    newSquare.classList.add(`square-{num}`);
+    newSquare.classList.add("highlight");
+    newSquare.style.backgroundColor = "{color}";
+    newSquare.style.opacity = 0.5
+    newSquare.dataset.testElement = "highlight"
+    document.querySelector("chess-board").appendChild(newSquare); 
+    """
+    return js
+
